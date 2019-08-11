1254e6031b5e424f1e0d4fba431286139840a057
Mahmoud Kanani <mahmoud.kanani@damacgroup.com>
2019-08-12 00:35:56 +0400

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site!</p>
 
     <FaGulp />
   </Layout>
