7ba5d51d931c5113a36ecc644c56abeea9f23f16
Mahmoud Kanani <mahmoud.kanani@damacgroup.com>
2019-08-12 14:59:28 +0400

    Created the hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,30 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 
-import SEO from "../components/seo"
+import SEO from "../components/seo";
 
-import { FaGulp } from 'react-icons/fa';
+import BackgroundSection from '../components/Globals/BackgroundSection';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site!</p>
 
-    <FaGulp />
+    <BackgroundSection img={data.img.childImageSharp.fluid} title="MK Coffee" styleClass="default-background" />
   </Layout>
 )
+
+export const query = graphql`
+{
+  img: file(relativePath: {eq: "default-background.jpeg"}) {
+    childImageSharp {
+      fluid {
+        src
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
